65036aac2851123306b71ad61498689dc13e11f6
Eveyn-6 <selenaliot@yahoo.com>
2020-10-13 14:21:33 +0200

    first commit

---
+++ JsJ.jsx
@@ -0,0 +1,9 @@
+$('#couleur-fond').change(function() {
+    var cf = $('#couleur-fond option:selected').val();
+    $('#contenu').css('background-color', cf);
+  });
+  
+  $('#couleur-fond').change(function(){
+
+  }
+  )
